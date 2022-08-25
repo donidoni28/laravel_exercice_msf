3efef7961058916812fb8608c7bac38357fba70f
donidoni28 <liridon.cerimi@hotmail.com>
2022-08-25 12:25:54 +0200

    adding batiments, formations, typeformations, eleves

---
+++ app/Http/Controllers/TypeformationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Typeformation;
+use Illuminate\Http\Request;
+
+class TypeformationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Typeformation  $typeformation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Typeformation $typeformation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Typeformation  $typeformation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Typeformation $typeformation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Typeformation  $typeformation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Typeformation $typeformation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Typeformation  $typeformation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Typeformation $typeformation)
+    {
+        //
+    }
+}
